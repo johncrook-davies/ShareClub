120f9ac7a3686f13570a37175dbd37f63b5807bf
Johnathan Crook-Davies <johnathanmdavies@gmail.com>
2020-03-29 16:00:07 +0100

    Added documentation to synchroniser functions.

--- synchroniser/index.js
+++ synchroniser/index.js
@@ -1,22 +1,30 @@
 import SQLite from "react-native-sqlite-storage";
-
 import { migrate } from './migrate';
 import { handleError } from './errors';
 
 SQLite.enablePromise(true);
-
 let db;
 
 export default class syncroniser {
     constructor(props, debug){
         this.props = props;
         this.dg = debug;
-        SQLite.DEBUG(debug);
+        //SQLite.DEBUG(debug); // Uncomment to allow sqlite debug info
     }
     initDb() {
+        /*
+            Performs an echotest to ensure that 
+            the SQLite library is present. If so
+            it opens the database. It checks the 
+            version of the database against the 
+            current version of the schema. If the
+            versions are different then it performs
+            a migration.
+        */
         const { database, size, schema } = this.props;
         const { version } = schema;
         SQLite.echoTest().then(() => {
+            // Open database
             SQLite.openDatabase(
                 database,
                 version,
@@ -25,7 +33,7 @@ export default class syncroniser {
             ).then((DB) => {
                 db = DB
                 this.dg && console.log("Database OPENED");
-                // Check database version and if diff migrate
+                // Check database version and if different migrate
                 db.executeSql('SELECT version FROM version WHERE version_id=1;').then(([v]) =>{
                     this.dg && console.log(`Current database version is`,v.rows.item(0).version)
                     this.dg && console.log(`Schema version is`,version)
@@ -41,6 +49,11 @@ export default class syncroniser {
         }).catch(error => handleError(error));
     };
     close() {
+        /*
+            Checks if the database is non-null
+            If it is then close it otherwise 
+            log a message
+        */
         if (db) {
             db.close().then(() => {
                 this.dg && console.log("Database CLOSED");
