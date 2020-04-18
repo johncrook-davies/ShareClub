9e01bef0e4b8ea2b8c25c29e6d2a90af4b7b69be
Johnathan Crook-Davies <johnathanmdavies@gmail.com>
2020-04-18 15:17:13 +0100

    Added debug logging to sync with db functions.

--- sync_with_server.js
+++ sync_with_server.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const url = 'https://warm-mesa-02274.herokuapp.com';
   
+const log = (arg) => (__DEV__ && console.log(`sync_with_server -> ${arg}`))
+
 const handleApiError = (error) => {
     switch(error.response.status) {
         case 404:
@@ -14,29 +16,47 @@ const handleApiError = (error) => {
 export const getExchanges = () => {
     return new Promise(resolve => {
         axios.get(`${url}/exchanges`)
-            .then(result => resolve(result.data))
+            .then(result => {
+                log(`getStocks: got exchanges`)
+                resolve(result.data)
+            })
     })
 }
 
 export const getStocks = () => {
     return new Promise(resolve => {
         axios.get(`${url}/stocks`)
-            .then(result => resolve(result.data))
+            .then(result => {
+                log(`getStocks: got stocks`)
+                resolve(result.data)
+            })
     })
 }
 
 export const getExchange = (ex) => {
     return new Promise((resolve,reject) => {
         axios.get(`${url}/exchanges/${ex}`)
-            .then(result => resolve(result.data))
-            .catch(error => reject(handleApiError(error)))
+            .then(result => {
+                log(`getExchange: got exchange ${ex}`)
+                resolve(result.data)
+            })
+            .catch(error => {
+                log(`getExchange: could not find exchange ${ex}`)
+                reject(handleApiError(error))
+            })
     })
 }
 
 export const getStock = (stock) => {
     return new Promise((resolve,reject) => {
         axios.get(`${url}/stocks/${stock}`)
-            .then(result => resolve(result.data))
-            .catch(error => reject(handleApiError(error)))
+            .then(result => {
+                log(`getStock: got stock ${stock}`)
+                resolve(result.data)
+            })
+            .catch(error => {
+                log(`getExchange: could not find stock ${stock}`)
+                reject(handleApiError(error))
+            })
     })
 }
