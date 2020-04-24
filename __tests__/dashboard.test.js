e5bb1cae07985819f44d7f1d9c9eb1a4682f3e3d
Johnathan Crook-Davies <johnathanmdavies@gmail.com>
2020-04-24 10:25:13 +0100

    Set up tests for dashboard.

--- __tests__/dashboard.test.js
+++ __tests__/dashboard.test.js
@@ -1,8 +1,16 @@
 import 'react-native';
+import {
+    SafeAreaView,
+    Text
+} from 'react-native';
 import React from 'react';
 import { syncdb, store } from './setupTests';
 import { Provider } from 'react-redux';
-import renderer from 'react-test-renderer';
+import { 
+    create,
+    act,
+    findAllInRenderedTree
+} from 'react-test-renderer';
 
 import Dashboard from '../views/dashboard';
 
@@ -12,11 +20,22 @@ beforeAll(() => spy = jest.spyOn(syncdb, 'get'))
 beforeEach(() => spy.mockClear())  
 
 describe('General behaviour', () => {
-    let wrapper;
+    let wrp,
+        rendered;
     beforeEach(() => {
-        wrapper = renderer.create(<Provider store={store}><Dashboard/></Provider>);
+        act(() => {
+            wrp = create(<Provider store={store}><Dashboard/></Provider>);
+        })
+        rendered = wrp.root.findByType(SafeAreaView);
     })
-    it('renders correctly', () => {
-        expect(spy).toHaveBeenCalled()
+    it('renders with more than one children', () => {
+        expect(
+            rendered.props.children.length
+        ).toBeGreaterThan(1);
+    });
+    it('calls get', () => {
+        expect(
+            spy
+        ).toHaveBeenCalledWith({"all": "clubs"});
     });
 })
