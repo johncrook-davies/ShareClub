ae3ba53e59cd82b84d9c415f59ea6618fd3ff72c
Johnathan Crook-Davies <johnathanmdavies@gmail.com>
2020-05-06 08:44:51 +0100

    Added stocks to index view.

--- views/investments/index_view.js
+++ views/investments/index_view.js
@@ -20,26 +20,48 @@ import AssetSummary from './asset_summary';
 
 const Index = ({ db, route, navigation }) => {
   const cs = useColorScheme(),
-        [ind, setInd] = useState([{name: 'Loading...'}]),
+        [ind, setInd] = useState({name: 'Loading...'}),
         [stocks, setStocks] = useState([]);
-  console.log(ind)
+  //console.log(ind)
   // Load data from database on initialisation
   useEffect(() => {
+    var stockList = [];
     if(db.readyState === 'ready') {
       db.call.get({all: 'indices', where: {symbol: {isEqualTo: route.params.symbol}}})
-        .then((r)=> {
+        .then(([r])=> {
           setInd(r)
           return r
         })
         .then((r) => {
-          
+          let arr = JSON.parse(r.stocks);
+          arr.map((s)=> {
+            db.call.get({all: 'stocks', where: {symbol: {isEqualTo: s}}})
+              .then(([r])=> {
+                stockList.push(r)
+                if(arr.indexOf(s) === arr.length -1) {
+                  setStocks(stockList)
+                }
+              })
+              .catch((e)=> {
+                console.log(`AssetSummary -> ${e}`)
+              })
+          })
         })
         .catch((e)=> {console.log(`AssetSummary -> ${e}`)})
     }
   },[db.readyState])
-
   return <Div cs={cs}>
-    <H1 cs={cs}>{`Index is ${ind[0].name}`}</H1>
+    {stocks.map((s) => 
+      <AssetSummary
+        type={ 'Stock' }
+        cs={ cs }
+        key={ s.name }
+        name={ s.name }
+        symbol={ s.symbol }
+        latest_price={ s.latest_price }
+        navigation={ navigation }
+        />
+    )}
   </Div>
 }
 
